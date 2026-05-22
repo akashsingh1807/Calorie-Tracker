80bd3c209dc50abb8d9a521132a6857e1021fba1
Akashsingh01 <engg.akashsingh@gmail.com>
2026-05-23 03:50:18 +0530

    Commit UI updates

--- android/composeApp/build/tmp/kapt3/stubs/debug/com/calorie/tracker/Screen.java
+++ android/composeApp/build/tmp/kapt3/stubs/debug/com/calorie/tracker/Screen.java
@@ -1,10 +1,19 @@
 package com.calorie.tracker;
 
-@kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000\"\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0002\b\u0006\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0000\b7\u0018\u00002\u00020\u0001:\u0005\u0003\u0004\u0005\u0006\u0007B\u0007\b\u0004\u00a2\u0006\u0002\u0010\u0002\u0082\u0001\u0005\b\t\n\u000b\f\u00a8\u0006\r"}, d2 = {"Lcom/calorie/tracker/Screen;", "", "()V", "Auth", "DailyGoals", "Dashboard", "Onboarding", "Streak", "Lcom/calorie/tracker/Screen$Auth;", "Lcom/calorie/tracker/Screen$DailyGoals;", "Lcom/calorie/tracker/Screen$Dashboard;", "Lcom/calorie/tracker/Screen$Onboarding;", "Lcom/calorie/tracker/Screen$Streak;", "composeApp_debug"})
+@kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u00002\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0002\b\n\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0000\b7\u0018\u00002\u00020\u0001:\t\u0003\u0004\u0005\u0006\u0007\b\t\n\u000bB\u0007\b\u0004\u00a2\u0006\u0002\u0010\u0002\u0082\u0001\t\f\r\u000e\u000f\u0010\u0011\u0012\u0013\u0014\u00a8\u0006\u0015"}, d2 = {"Lcom/calorie/tracker/Screen;", "", "()V", "Account", "Auth", "DailyGoals", "Dashboard", "Onboarding", "Reminders", "Streak", "WaterTracker", "WeightTracker", "Lcom/calorie/tracker/Screen$Account;", "Lcom/calorie/tracker/Screen$Auth;", "Lcom/calorie/tracker/Screen$DailyGoals;", "Lcom/calorie/tracker/Screen$Dashboard;", "Lcom/calorie/tracker/Screen$Onboarding;", "Lcom/calorie/tracker/Screen$Reminders;", "Lcom/calorie/tracker/Screen$Streak;", "Lcom/calorie/tracker/Screen$WaterTracker;", "Lcom/calorie/tracker/Screen$WeightTracker;", "composeApp_debug"})
 public abstract class Screen {
     
     private Screen() {
         super();
+    }
+    
+    @kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000\f\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\b\u00c7\u0002\u0018\u00002\u00020\u0001B\u0007\b\u0002\u00a2\u0006\u0002\u0010\u0002\u00a8\u0006\u0003"}, d2 = {"Lcom/calorie/tracker/Screen$Account;", "Lcom/calorie/tracker/Screen;", "()V", "composeApp_debug"})
+    public static final class Account extends com.calorie.tracker.Screen {
+        @org.jetbrains.annotations.NotNull()
+        public static final com.calorie.tracker.Screen.Account INSTANCE = null;
+        
+        private Account() {
+        }
     }
     
     @kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000\f\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\b\u00c7\u0002\u0018\u00002\u00020\u0001B\u0007\b\u0002\u00a2\u0006\u0002\u0010\u0002\u00a8\u0006\u0003"}, d2 = {"Lcom/calorie/tracker/Screen$Auth;", "Lcom/calorie/tracker/Screen;", "()V", "composeApp_debug"})
@@ -43,12 +52,39 @@ public abstract class Screen {
         }
     }
     
+    @kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000\f\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\b\u00c7\u0002\u0018\u00002\u00020\u0001B\u0007\b\u0002\u00a2\u0006\u0002\u0010\u0002\u00a8\u0006\u0003"}, d2 = {"Lcom/calorie/tracker/Screen$Reminders;", "Lcom/calorie/tracker/Screen;", "()V", "composeApp_debug"})
+    public static final class Reminders extends com.calorie.tracker.Screen {
+        @org.jetbrains.annotations.NotNull()
+        public static final com.calorie.tracker.Screen.Reminders INSTANCE = null;
+        
+        private Reminders() {
+        }
+    }
+    
     @kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000\f\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\b\u00c7\u0002\u0018\u00002\u00020\u0001B\u0007\b\u0002\u00a2\u0006\u0002\u0010\u0002\u00a8\u0006\u0003"}, d2 = {"Lcom/calorie/tracker/Screen$Streak;", "Lcom/calorie/tracker/Screen;", "()V", "composeApp_debug"})
     public static final class Streak extends com.calorie.tracker.Screen {
         @org.jetbrains.annotations.NotNull()
         public static final com.calorie.tracker.Screen.Streak INSTANCE = null;
         
         private Streak() {
+        }
+    }
+    
+    @kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000\f\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\b\u00c7\u0002\u0018\u00002\u00020\u0001B\u0007\b\u0002\u00a2\u0006\u0002\u0010\u0002\u00a8\u0006\u0003"}, d2 = {"Lcom/calorie/tracker/Screen$WaterTracker;", "Lcom/calorie/tracker/Screen;", "()V", "composeApp_debug"})
+    public static final class WaterTracker extends com.calorie.tracker.Screen {
+        @org.jetbrains.annotations.NotNull()
+        public static final com.calorie.tracker.Screen.WaterTracker INSTANCE = null;
+        
+        private WaterTracker() {
+        }
+    }
+    
+    @kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000\f\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\b\u00c7\u0002\u0018\u00002\u00020\u0001B\u0007\b\u0002\u00a2\u0006\u0002\u0010\u0002\u00a8\u0006\u0003"}, d2 = {"Lcom/calorie/tracker/Screen$WeightTracker;", "Lcom/calorie/tracker/Screen;", "()V", "composeApp_debug"})
+    public static final class WeightTracker extends com.calorie.tracker.Screen {
+        @org.jetbrains.annotations.NotNull()
+        public static final com.calorie.tracker.Screen.WeightTracker INSTANCE = null;
+        
+        private WeightTracker() {
         }
     }
 }
